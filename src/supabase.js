60c0c98366514d2b25f596bc9507e0c6f722609d
KellySitta <kellybarbosa5341@gmail.com>
2025-11-12 22:00:23 -0300

    Update supabase.js
    Banco de dados

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pccemlphvvtnfswzunsm.supabase.co'; //
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjY2VtbHBodnZ0bmZzd3p1bnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NzYxOTksImV4cCI6MjA3ODU1MjE5OX0.2gQfO7Z3vG5zY-M0wiwwUXdJH_Lu888U7q6z3i4IPsI'; //
+const supabaseUrl = 'https://pccemlphvvtnfswzunsm.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjY2VtbHBodnZ0bmZzd3p1bnNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NzYxOTksImV4cCI6MjA3ODU1MjE5OX0.2gQfO7Z3vG5zY-M0wiwwUXdJH_Lu888U7q6z3i4IPsI';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
